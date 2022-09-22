7e847aa92aad6cd35722a2935a8d1c569b62c6d2
cleverplant <cleverplant39@gmail.com>
2022-09-22 11:00:45 +0200

    все подчистил - в задаче 15 прикрутил while

--- корзина/проверка while/Program.cs
+++ корзина/проверка while/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Console.Write("введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+int N = 4;
+int run = 1;
+while(run <= N - 1)
+{
+    // if(run % 2 == 0)
+    // Console.WriteLine(run + " рабочие день");
+    run = run + 1;
+}
+Console.Write(run + " <= ");
+
+
