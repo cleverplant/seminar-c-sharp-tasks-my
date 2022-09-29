60b8ddce89105117e8870a7a9c7289930a825682
cleverplant <cleverplant39@gmail.com>
2022-09-29 22:59:34 +0200

    потом допилить 29 задачу - прибавит рандом

---
+++ Библиотека/функция задаёт массив из N элементов и выводит их на экран/randomArray.cs
@@ -0,0 +1,22 @@
+﻿/*
+ задаёт массив из N элементов и выводит их на экран.
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33]
+*/
+
+int lenArray = ReadConsoleInput("введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+
+// Библиотека\функция ввода сообщения
+int ReadConsoleInput(string StringNumber)
+{
+    Console.Write(StringNumber);
+    return Convert.ToInt32(Console.ReadLine());
+}
--- Задача 29/Program29.cs
+++ Задача 29/Program29.cs
@@ -45,5 +45,21 @@ PrintArray(arr);
 SelectionSort(arr);
 PrintArray(arr);
 
+// надо было прикрутить, но времени не было... 
+// ----------------------------------------------------
+//  задаём массив из N элементов и выводим их на экран.
+// 5 -> [1, 2, 5, 7, 19]
+// 3 -> [6, 1, 33]
+
+
+// int lenArray = ReadConsoleInput("введите длинну массива: ");
+
+// int[] randomArray = new int[lenArray];
+// for (int i = 0; i < randomArray.Length; i++)
+// {
+//     randomArray[i] = new Random().Next(1,9);
+//     Console.Write(randomArray[i] + " ");
+// }
+//------------------------------------------------------
 
 
