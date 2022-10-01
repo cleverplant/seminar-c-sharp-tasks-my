f6591bd963e30a74324f8de30d23804069470388
cleverplant <cleverplant39@gmail.com>
2022-10-01 18:28:06 +0200

    magic litle

--- Библиотека реал/Max_Min_Array_Value/MaxMinArrayValue.cs
+++ Библиотека реал/Max_Min_Array_Value/MaxMinArrayValue.cs
@@ -1,5 +1,7 @@
 ﻿double[] numbers = { 3.1, 21.9, 7.0, 4.7, 22.07, 93.7777, 2.1, 71.4, 78.5, 24.3 };
 
+void MaxMinArray()
+{
 int size = numbers.Length;
 //double[] numbers = new double[size];/* массив double создан */
 
@@ -28,5 +30,10 @@ for (int i = 0; i < numbers.Length; i++)
 }
 Console.WriteLine($"max значение  {max} -> у него индекс {max_ind} ");
 Console.WriteLine($"мин. значение  {min} -> у него индекс {min_ind}");
+}
+ 
+MaxMinArray();
+
+
 
 
