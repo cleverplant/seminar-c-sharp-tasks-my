701810228e836636dffcdde28e2395b31ada6555
cleverplant <cleverplant39@gmail.com>
2022-10-07 03:43:35 +0200

    добавил подробные коменты

--- Библиотека парсинг по API/parsing weather C#/GetRequest.cs
+++ Библиотека парсинг по API/parsing weather C#/GetRequest.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿/*
+класс WebRequest - отправка запроса
+Класс WebResponse - получение ответа на запрос
+*/
 using System.Net;
 
 
@@ -11,27 +12,35 @@ namespace OpenweathermapApi
         HttpWebRequest _request;
         string _address;
 
-        public string Response { get; set; }
+        //5). Создадим метод Response: в этот метод будем записывать ответ от вебсервера
+        public string Response { get; set; }// ---> !!! Перейдем в class Program Weather.cs !!! 
 
-        public GetRequest(string address)
+        // Для выполнения Get запроса создаем новый класс - public class GetRequest 
+        public GetRequest(string address)// 1). Создадим конструктор класса в который передаем строку класса:
         {
-            _address = address;
+            _address = address;// 
         }
 
-        public void Run()
+        public void Run()//  2). Создадим паблик метод Run. Этот метод в нужный момент запускает созданный запрос
         {
-            _request = (HttpWebRequest)WebRequest.Create(_address);
-            _request.Method = "Get";
+            //создаем запрос: _request = WebRequest.Create();
+            _request = (HttpWebRequest)WebRequest.Create(_address);// 3).  создадим объект запроса - _request в HttpWebRequest
+
+            _request.Method = "Get";// данный запрос является Get запросом. 
 
             try
-            {
+            {   // 4). Создадим объект ответа вебсервера:
                 HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
                 var stream = response.GetResponseStream();
+
                 if (stream != null) Response = new StreamReader(stream).ReadToEnd();
             }
             catch (Exception)
-            {                
+            {
             }
         }
     }
 }
+// Перейдем в class Program Weather.cs -> Библиотека парсинг по API\parsing weather C#\Weather.cs
+
+
--- Библиотека парсинг по API/parsing weather C#/Weather.cs
+++ Библиотека парсинг по API/parsing weather C#/Weather.cs
@@ -1,14 +1,10 @@
 ﻿/*
-https://www.telerik.com/fiddler
-https://openweathermap.org/current#current_JSON
-https://jsoneditoronline.org/#left=local.tukeyo
-https://marketplace.visualstudio.com/items?itemName=patcx.vscode-nuget-gallery
-
 класс WebRequest - отправка запроса
 Класс WebResponse - получение ответа на запрос
 */
+
+//Перейдем в class Program Weather.cs
 using Newtonsoft.Json.Linq;
-using System;
 
 namespace OpenweathermapApi
 {
@@ -16,19 +12,22 @@ namespace OpenweathermapApi
     {
         static void Main(string[] args)
         {
-
+            // 6). Cоздаем запрос к веб порталу: В созданную переменную var request нужно передать адрес нашего вебсайта
             var request = new GetRequest($"http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=0a9b051464990ec3facc0b5dba7c7c73");
-            request.Run();
+            
+            request.Run();// 7). Чтобы выполнить данный запрос, нужно у объекта request выполнить метод RUN
 
-            var response = request.Response;
+            var response = request.Response; //
 
+
+            // 8). нам нужно обработать полученный ответ. Для этого создаем переменную в которую пишется парсинг:
             var json = JObject.Parse(response);// на этом этапе надо сделать запрос и проверить ответ в программе fiddler
 
             /* нужно обратить внимание, что переменные в строке JSON имеют "вложенность", 
             например параметр humidity или temp вложены в main, поэтому main["humidity"]; или main["temp"];, но 
             сначала нужно сделать запрос JSON к глвному параметру json["main"];*/
 
-            var name = json["name"];
+            var name = json["name"]; // 9). из переменной var json вытаскиваем нужные нам значения парсинга:
             var main = json["main"];
             var wind = json["wind"];
 
