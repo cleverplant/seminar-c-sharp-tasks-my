8a458066e62361398db2ecd495f3c5d71376ac24
cleverplant <cleverplant39@gmail.com>
2022-10-12 20:31:44 +0200

    сломал что ли

--- Задача 58/Program58.cs
+++ Задача 58/Program58.cs
@@ -57,17 +57,6 @@ void SpiralArray(int[,] array)
     }
 }
 
-void FillArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-        }
-    }
-}
-
 void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
