b94de0177b39779df2a06f30abf1a245d8c68884
cleverplant <cleverplant39@gmail.com>
2022-10-15 23:34:50 +0200

    последнее ДЗ -вот и кончилось лето

---
+++ Задача 64/Program64.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 64: Задайте значения M и N. Напишите рекурсивный метод, 
+который выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+M = 1; N = 9. -> "3, 6, 9"          M = 13; N = 20. -> "15, 18"
+*/
+
+void Recursion() // смысл - ОТКАЗАТЬСЯ ОТ ГЛОБАЛЬНЫХ ДАННЫХ (переменных) !!!
+{
+    Console.WriteLine();
+    Console.WriteLine("Задайте значение m : ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Задайте значение n : ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    Natural(m, n);
+
+    void Natural(int n, int m)
+    {
+        m++;
+        if (m % 3 == 0)
+        {
+            Console.WriteLine(m);
+        }
+        if (m > n)
+        {
+            return;
+        }
+        Natural(n, m);
+    }
+    Console.WriteLine();
+}
+Recursion();
+
+
+
+
+
---
+++ Задача 66/Program66.cs
@@ -0,0 +1,54 @@
+﻿/*
+Задача 66:Задайте значения M и N. 
+Напишите рекурсивный метод, который найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+
+Console.WriteLine();
+Console.WriteLine("Задайте значение m : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение n : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine(TaskNaturSum(m, n));
+
+int TaskNaturSum(int m, int n, int sum = 0)
+{
+    if (m > n)
+    {
+        return sum;
+    }
+    sum += m;
+    m++;
+    return TaskNaturSum(m, n, sum);
+}
+
+/*
+Задача 67:Напишите программу, 
+которая будет принимать на вход число и возвращать сумму его цифр.
+
+453 -> 12
+45 -> 9
+*/
+void task67()
+{
+    Console.WriteLine("Задайте число : ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(SumNum(number));
+
+    int SumNum(int num, int sum = 0)
+    {
+        if (num == 0)
+        {
+            return sum;
+        }
+        sum = sum + num % 10;
+        num = num / 10;
+        return SumNum(num, sum);
+
+    }
+}
+//task67();
